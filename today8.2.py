17ff25d2e92eadda69757b2319d2b8bb042ce0d9
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2023-11-08 23:56:00 +0530

    Update today8.2.py

--- today8.2.py
+++ today8.2.py
@@ -114,6 +114,151 @@
 			|write mode|
 
 
+# x = open('testFor8.2.txt','w')
+# x.write('Hello World!')
+# # we must close it now
+# x.close()
+
+# ** be carefull with that 
+# 	** because now all of details are remove from testFor8.2 file and
+# 		now it has only 'Hello World!'
+# 		## remember these
+	
+	# testFor8.2 file 
+
+	# 	Hello World!
+		
+# 	# -------------
+# x = open('testFor8.2.txt','w')
+# x.write('Hello World!')
+# x.write('Welcome')
+# x.close()
+
+	# now testFor8.2 file has
+
+# 		Hello World!Welcome
+
+# we can fix these problem using 
+# 	we can get these in new line using by newline charactor 
+
+# x = open('testFor8.2.txt','w')
+# x.write('Hello World!\n')
+# x.write('Welcome')
+# x.close()
+
+# 	# now testFor8.2 file has
+
+# 		Hello World!
+# 		Welcome
+
+# x = open('testFor8.2.txt','w')
+# x.write('Hello World!\n \n')
+# x.write('Welcome')
+# x.close()
+
+# 	# now testFor8.2 file has	
+
+# 		Hello World!
+ 
+# 		Welcome
+
+
+	# ---------------------
+
+	# **** if you want to let before things 
+	#  	you can use || append || mode
+
+				# |append mode|
+
+
+# 	if testFor8.2.txt file has
+# 		Name = Sumathipala
+# 		Age = 50
+
+# x = open('testFor8.2.txt','a')
+# x.write('hello Sumathi')
+# x.close()
+
+# >> now it has testFor8.2.txt file
+# 		Name = Sumathipala
+# 		Age = 50
+# 		hello Sumathi
+
+
+
+# x = open('testFor8.2.txt','a')
+# x.write('hello Sumathi')
+# x.close()
+
+# >>> these file has now 
+# 		Name = Sumathipala
+# 		Age = 50
+# 		hello Sumathihello Sumathi
+
+# ** we can use the new line charactor with like this
+
+# x = open('testFor8.2.txt','a')
+# x.write('\nhello Sumathi')
+# x.close()
+
+# ** now this file has 
+# Name = Sumathipala
+# Age = 50
+# hello Sumathihello Sumathi
+# hello Sumathi
+
+# -----------------------------
+
+# x = open('hello.txt','a')
+# **** if there is not hello.txt file but it is auto created now ***
+
+# -----------------
+
+# ** how to open image -------
+# x = open('nature.JPG','rb')
+# print(x.read())
+# 	## rb = readin binary 
+# >>> image eka open wenne binary akarayen we
+
+
+# -----------------------------------------------------------------------------------
+	
+				# |OS module|
+
+import os
+
+
+## (1) how to remove a file 
+# os.remove('hello.txt')
+
+### now the hello.txt file is removed 
+
+
+## (2) how to rename a file
+# os.rename('before_name','new_name')
+
+
+## (3) how to know if this file exists 
+	# මෙම ගොනුව තිබේදැයි දැන ගන්නේ කෙසේද
+# print(os.path.exists('test.txt'))
+
+	## you can show the out put is True or False
+
+## (4) how to find the path of file
+# print(os.path.abspath('test.py'))
+
+
+## (5) how to know the size of file
+# print(os.path.getsize('today8.2.py'))	
+# >>> 5077 
+
+	## 5077 is the number of element of these file
+
+	
+
+
+
+
 
 		
 
