5e05378b9b851e72ba6f200d2ea85a76a5ab66e0
IEPrasad <126505521+IEPrasad@users.noreply.github.com>
2023-11-13 10:48:40 +0530

    Add files via upload

---
+++ today13.py
@@ -0,0 +1,42 @@
+
+
+	|Vitual Environments in Python |  virtualenv |
+
+
+	virtual environments in Python provide a way to create isolated and self-contained 
+	environments for Python projects. They allow you to manage dependencies and 
+	packages separately for different projects, preventing conflicts and ensuring 
+	that each project has its own set of libraries. Virtual environments are crucial 
+	for maintaining a clean and organized development environment, enhancing 
+	reproducibility, and simplifying package management in Python projects. 
+	The venv module, virtualenv package, and conda are common tools used to create 
+	and manage virtual environments in Python.
+
+
+	
+1) make folder for the project
+2) command prompt 
+	cmd: pip install virtualenv
+3) go to the project folder
+4) make the virtual environment
+	cmd: virtualenv (virtual_environment folder name)
+	ex:
+		cmd virtualenv env1
+
+
+		more : https://youtu.be/kv2JJ1GPrdo?feature=shared
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
