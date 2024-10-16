298a16f4cff22e06ff418ec6014afe7a25e0b2ad
J.V.G.I.E Prasad <ieprasad23@gmail.com>
2024-10-16 23:40:02 +0530

    Creating a file using a python

---
+++ Python/file-creating.py
@@ -0,0 +1,24 @@
+# Create and write to the .txt file 
+with open("newtextfile.txt", "w") as file:
+  file.write("This is sample text.\n")
+  file.write("We can add multiple lines of data here.\n")
+
+# Open the file to read and display the contents 
+with open("newtextfile.txt", "r") as file:
+  content - file.read()
+  print("File content:\n", content)
+
+
+# Check the details such as name, mode, and whether it is closed
+
+print("\nFile details:")
+print("File name:", file.name)
+print("File mmode", file.mode)
+print("Is File closed:", file.closed)
+
+
+# Display file size
+import os
+file_size = os.path.getsize("newtextfile.txt")
+print("File size:", file_size, "bytes")
+
